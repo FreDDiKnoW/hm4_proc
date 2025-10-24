638aa1cbfa3685ef597ae218d53d5d793fd4b451
FreDDiKnoW <klumenko356@gmail.com>
2025-10-24 15:11:54 +0000

    more tests

--- tests/test_main.c
+++ tests/test_main.c
@@ -4,6 +4,39 @@
 #include "../include/bisection.h"
 #include "../include/array_utils.h"
 
+void test_bisection_edge_cases() {
+    printf("Running test 'test_bisection_edge_cases'...\n");
+    double tolerance = 0.00001;
+    double expected_root1 = 0.619061;
+    double expected_root2 = 1.512135;
+    double a_near = 1.51213;
+    double b_far = 1.6;
+    double actual_root_near_a = find_intersection(a_near, b_far, 0.0000001);
+    printf("Test 'root_near_a': a=%f, b=%f, result=%f\n", a_near, b_far, actual_root_near_a);
+    assert(fabs(actual_root_near_a - expected_root2) < tolerance);
+    printf("Test 'root_near_a' - SUCCEED\n");
+
+    double a_far = 0.6;
+    double b_near = 0.61907;
+    double actual_root_near_b = find_intersection(a_far, b_near, 0.0000001);
+    printf("Test 'root_near_b': a=%f, b=%f, result=%f\n", a_far, b_near, actual_root_near_b);
+    assert(fabs(actual_root_near_b - expected_root1) < tolerance);
+    printf("Test 'root_near_b' - SUCCEED\n");
+
+    double a_break = 0.6125; 
+    double b_break = 0.625;  
+    double accuracy_large = 0.001;
+    double expected_c = (a_break + b_break) / 2.0; 
+    double actual_root_break = find_intersection(a_break, b_break, accuracy_large);
+    
+    printf("Test 'break_on_accuracy': a=%f, b=%f, accuracy=%f\n", a_break, b_break, accuracy_large);
+    printf("expected_c = %f, actual_result = %f\n", expected_c, actual_root_break);
+    assert(fabs(actual_root_break - expected_c) < tolerance); 
+    printf("Test 'break_on_accuracy' - SUCCEED\n");
+    
+    printf("Test 'test_bisection_edge_cases' - SUCCEED\n");
+}
+
 void test_find_intersection() {
     printf("Running test 'test_find_intersection'...\n");
     double tolerance = 0.001;
@@ -62,6 +95,7 @@ int main() {
     printf("========== Starting All Tests ==========\n\n");
     
     test_find_intersection();
+    test_bisection_edge_cases();
     printf("\n");
     test_rotate_array();
     printf("\n");
