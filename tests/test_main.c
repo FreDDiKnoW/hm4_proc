e4a717925b33db4baa53b665f587ada7dd7b9516
FreDDiKnoW <klumenko356@gmail.com>
2025-12-05 11:10:39 +0000

    More tests for bisection and array + deleted unnecesary binary file

--- tests/test_main.c
+++ tests/test_main.c
@@ -1,38 +1,45 @@
 #include <stdio.h>
 #include <assert.h>
 #include <math.h>
+#include <string.h>
 #include "../include/bisection.h"
 #include "../include/array_utils.h"
 
 void test_bisection_edge_cases() {
     printf("Running test 'test_bisection_edge_cases'...\n");
+
     double tolerance = 0.00001;
     double expected_root1 = 0.619061;
     double expected_root2 = 1.512135;
+
     double a_near = 1.51213;
     double b_far = 1.6;
     double actual_root_near_a = find_intersection(a_near, b_far, 0.0000001);
-    printf("Test 'root_near_a': a=%f, b=%f, result=%f\n", a_near, b_far, actual_root_near_a);
+    
+    printf("  Test 'root_near_a': a=%f, b=%f, result=%f\n", a_near, b_far, actual_root_near_a);
     assert(fabs(actual_root_near_a - expected_root2) < tolerance);
-    printf("Test 'root_near_a' - SUCCEED\n");
+    printf("  Test 'root_near_a' - SUCCEED\n");
 
     double a_far = 0.6;
     double b_near = 0.61907;
     double actual_root_near_b = find_intersection(a_far, b_near, 0.0000001);
-    printf("Test 'root_near_b': a=%f, b=%f, result=%f\n", a_far, b_near, actual_root_near_b);
+    
+    printf("  Test 'root_near_b': a=%f, b=%f, result=%f\n", a_far, b_near, actual_root_near_b);
     assert(fabs(actual_root_near_b - expected_root1) < tolerance);
-    printf("Test 'root_near_b' - SUCCEED\n");
+    printf("  Test 'root_near_b' - SUCCEED\n");
 
     double a_break = 0.6125; 
     double b_break = 0.625;  
-    double accuracy_large = 0.001;
+    double accuracy_large = 0.001; 
     double expected_c = (a_break + b_break) / 2.0; 
+    
+    // h(c) < accuracy_large
     double actual_root_break = find_intersection(a_break, b_break, accuracy_large);
     
-    printf("Test 'break_on_accuracy': a=%f, b=%f, accuracy=%f\n", a_break, b_break, accuracy_large);
-    printf("expected_c = %f, actual_result = %f\n", expected_c, actual_root_break);
+    printf("  Test 'break_on_accuracy': a=%f, b=%f, accuracy=%f\n", a_break, b_break, accuracy_large);
+    printf("  Expected c = %f, Actual result = %f\n", expected_c, actual_root_break);
     assert(fabs(actual_root_break - expected_c) < tolerance); 
-    printf("Test 'break_on_accuracy' - SUCCEED\n");
+    printf("  Test 'break_on_accuracy' - SUCCEED\n");
     
     printf("Test 'test_bisection_edge_cases' - SUCCEED\n");
 }
@@ -42,8 +49,10 @@ void test_find_intersection() {
     double tolerance = 0.001;
     double expected_root1 = 0.619;
     double expected_root2 = 1.512;
+    
     double actual_root1 = find_intersection(0.0, 1.0, 0.0001);
     double actual_root2 = find_intersection(1.0, 2.0, 0.0001);
+    
     assert(fabs(actual_root1 - expected_root1) < tolerance);
     assert(fabs(actual_root2 - expected_root2) < tolerance);
     printf("Test 'test_find_intersection' - SUCCEED\n");
@@ -67,6 +76,7 @@ void test_rotate_array() {
 
     int actual_rotated[N][N]; 
     rotate_array(original, actual_rotated);
+    
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
             assert(actual_rotated[i][j] == expected_rotated[i][j]);
@@ -76,6 +86,26 @@ void test_rotate_array() {
     printf("Test 'test_rotate_array' - SUCCEED\n");
 }
 
+void test_rotate_array_full_cycle() {
+    printf("Running test 'test_rotate_array_full_cycle'...\n");
+    
+    int original[N][N] = {
+        {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}
+    };
+    
+    int temp1[N][N], temp2[N][N];
+    rotate_array(original, temp1);
+    rotate_array(temp1, temp2);
+    rotate_array(temp2, temp1);
+    rotate_array(temp1, temp2);
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            assert(temp2[i][j] == original[i][j]);
+        }
+    }
+    printf("Test 'test_rotate_array_full_cycle' - SUCCEED\n");
+}
 
 void test_checksum_positive_numbers() {
     printf("Running test 'test_checksum_positive_numbers'...\n");
@@ -91,14 +121,33 @@ void test_checksum_mixed_numbers() {
     printf("Test 'test_checksum_mixed_numbers' - SUCCEED\n");
 }
 
+void test_checksum_edge_cases() {
+    printf("Running test 'test_checksum_edge_cases'...\n");
+    
+    int single_element[] = {42};
+    assert(calculate_checksum(single_element, 1) == 42);
+    
+    int zeros[] = {0, 0, 0};
+    assert(calculate_checksum(zeros, 3) == 0);
+
+    int empty[1] = {0}; 
+    assert(calculate_checksum(empty, 0) == 0);
+
+    printf("Test 'test_checksum_edge_cases' - SUCCEED\n");
+}
+
 int main() {
     printf("========== Starting All Tests ==========\n\n");
     
     test_find_intersection();
+    printf("\n");
+    
     test_bisection_edge_cases();
     printf("\n");
+    
     test_rotate_array();
     printf("\n");
+    
     test_checksum_positive_numbers();
     test_checksum_mixed_numbers();
     
